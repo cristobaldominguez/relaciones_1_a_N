7821bfe1908fce13288eb9518ae5ebfddd37cb12
Cristobal Dominguez <cristobald@gmail.com>
2022-06-28 20:38:12 -0400

    Cuarto Commit

--- app/controllers/apartments_controller.rb
+++ app/controllers/apartments_controller.rb
@@ -2,4 +2,32 @@ class ApartmentsController < ApplicationController
     def index
         @apartments = Apartment.all
     end
+
+    def show
+        @apartment = Apartment.find(params[:id])
+    end
+
+    def new
+        @apartment = Apartment.new
+        @building = Building.new
+        @buildings = Building.pluck(:name, :id)
+    end
+
+    def create
+        @apartment = Apartment.new(apartment_params)
+        
+        respond_to do |format| 
+            if @apartment.save
+                format.html { redirect_to @apartment, notice: 'Apartment was successfully created.'}
+            else
+                format.html { render :new }
+            end
+        end
+    end
+
+    private
+
+    def apartment_params
+        params.require(:apartment).permit(:number, :building_id)
+    end
 end
